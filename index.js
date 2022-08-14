e47c1a7e9510816430a9675aedfe5f260aef2288
fheezy <farhanhoque764@yahoo.com>
2022-08-14 14:06:15 -0500

    added a node mod. in index.js

--- index.js
+++ index.js
@@ -1,142 +1,132 @@
-const Logic = require('./lib/Logic');
-new Logic().iniMenu();
+// node modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateTeam = require("./pageTemplate");
+const logic = require("logic")
 
+// lib modules
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
+// Array for answers to questions
+const newStaffMemberData = [];
 
-
-
-
-
-
-
-// // node modules
-// const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generateTeam = require("./pageTemplate")
-
-// // lib modules
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
-
-// // Array for answers to questions
-// const newStaffMemberData = [];
-
-// // Questions asked on command line
-// const questions = async () => {
-//   const answers = await inquirer
-//     .prompt([
-//       {
-//         type: "input",
-//         message: "What is your name?",
-//         name: "name",
-//       },
-//       {
-//         type: "input",
-//         message: "What is your ID number?",
-//         name: "id",
-//       },
-//       {
-//         type: "input",
-//         message: "What is your email?",
-//         name: "email",
-//       },
-//       {
-//         type: "list",
-//         message: "What is your role?",
-//         name: "role",
-//         choices: ["Engineer", "Intern", "Manager"],
-//       },
-//     ])
+// Questions asked on command line
+const questions = async () => {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
 
 
     
-//     //  console.log(answers);
-//       // if manager selected, answer these question
-//       if (answers.role === "Manager") {
-//         const managerAns = await inquirer
-//           .prompt([
-//             {
-//               type: "input",
-//               message: "What is your office number",
-//               name: "officeNumber",
-//             },
-//           ])
-//           const newManager = new Manager(
-//             answers.name,
-//             answers.id,
-//             answers.email,
-//             managerAns.officeNumber
-//           );
-//           newStaffMemberData.push(newManager);
+    //  console.log(answers);
+      // if manager selected, answer these question
+      if (answers.role === "Manager") {
+        const managerAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your office number",
+              name: "officeNumber",
+            },
+          ])
+          const newManager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+          );
+          newStaffMemberData.push(newManager);
           
-//         // if engineer selected answer these questions
-//       } else if (answers.role === "Engineer") {
-//         const githubAns = await inquirer
-//           .prompt([
-//             {
-//               type: "input",
-//               message: "What is your GitHub user name?",
-//               name: "github",
-//             }
-//           ])
-//             const newEngineer = new Engineer(
-//               answers.name,
-//               answers.id,
-//               answers.email,
-//               githubAns.github
-//             );
-//             newStaffMemberData.push(newEngineer);
+        // if engineer selected answer these questions
+      } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your GitHub user name?",
+              name: "github",
+            }
+          ])
+            const newEngineer = new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              githubAns.github
+            );
+            newStaffMemberData.push(newEngineer);
           
-//         // if intern selected answer these questions
-//       } else if (answers.role === "Intern") {
-//         const internAns = await inquirer
-//           .prompt([
-//             {
-//               type: "input",
-//               message: "What university did you attend?",
-//               name: "school",
-//             },
-//           ])
+        // if intern selected answer these questions
+      } else if (answers.role === "Intern") {
+        const internAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What university did you attend?",
+              name: "school",
+            },
+          ])
           
-//           const newIntern = new Intern(
-//             answers.name,
-//             answers.id,
-//             answers.email,
-//             internAns.school
-//           );
-//           newStaffMemberData.push(newIntern);          
-//       } 
+          const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school
+          );
+          newStaffMemberData.push(newIntern);          
+      } 
 
-// }; //function towards the end of the question
+}; //function towards the end of the question
 
-// async function promptQuestions() {
-//   await questions()
+async function promptQuestions() {
+  await questions()
     
   
-//   const addMemberAns = await inquirer
-//     .prompt([
-//       {
-//         name:'addMember',
-//         type: 'list',
-//         choices: ['Add a new member', 'Create team'],
-//         message: "What would you like to do next?"
-//       }
-//     ])
+  const addMemberAns = await inquirer
+    .prompt([
+      {
+        name:'addMember',
+        type: 'list',
+        choices: ['Add a new member', 'Create team'],
+        message: "What would you like to do next?"
+      }
+    ])
 
-//     if (addMemberAns.addMember === 'Add a new member') {
-//       return promptQuestions()
-//     }
-//     return createTeam();
-// }  
+    if (addMemberAns.addMember === 'Add a new member') {
+      return promptQuestions()
+    }
+    return createTeam();
+}  
 
-// promptQuestions();
+promptQuestions();
 
-// function createTeam () {
-//   console.log("new guy", newStaffMemberData)
-//   fs.writeFileSync(
-//     "./dist/index.html",
-//     generateTeam(newStaffMemberData),
-//     "utf-8"
-//   );
-// }
+function createTeam () {
+  console.log("new guy", newStaffMemberData)
+  fs.writeFileSync(
+    "./dist/index.html",
+    generateTeam(newStaffMemberData),
+    "utf-8"
+  );
+}
