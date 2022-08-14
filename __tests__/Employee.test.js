8a5152e2df724dfe27e1e9a529115c96c1aae2f7
fheezy <farhanhoque764@yahoo.com>
2022-08-14 13:44:00 -0500

    changed inside text

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
-test("Can instantiate Employee instance", () => {
+test("Create employee object", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
 });
@@ -23,25 +23,25 @@ test("Can set email via constructor argument", () => {
   expect(e.email).toBe(testValue);
 });
 
-test("Can get name via getName()", () => {
+test("get employee name()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
 
-test("Can get id via getId()", () => {
+test("get employee id()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
-test("Can get email via getEmail()", () => {
+test("get employee email()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
+test("get employee role", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
